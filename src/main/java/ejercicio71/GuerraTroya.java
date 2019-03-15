0c4445f0d4c98ac17ca6e51a0ae0fd95683d1193
Alba <alba.rodriguezsa@elorrieta-errekamari.com>
2019-03-15 14:20:00 +0100

    ejer71

--- src/main/java/ejercicio71/GuerraTroya.java
+++ src/main/java/ejercicio71/GuerraTroya.java
@@ -7,13 +7,25 @@ public class GuerraTroya {
 		Griego griego1 = new Griego("Aquiles", 20, 8);
 		Griego griego2 =new Griego("Agamenon", 21, 9);
 		Griego griego3 =new Griego("Ajax", 23, 10);
+		
 		caballo1.montarseEnCaballo(griego1);
 		caballo1.montarseEnCaballo(griego2);
+		caballo1.montarseEnCaballo(griego3);
 		caballo1.montarseEnCaballo(griego3);
 		
 		for(int i=0; i<caballo1.getOcupacion();i++) {
 			caballo1.mostrarCaballo(i);
 		}
+		System.out.println("-----------------------");
+		Griego griego4 =new Griego();
+		caballo1.montarseEnCaballo(griego4);
+		for(int i=0; i<caballo1.getOcupacion();i++) {
+			caballo1.mostrarCaballo(i);
+		}
+		
+		System.out.println("-----------------------");
+		caballo1.buscar("Odiseo");
+		
 		
 	}
 
