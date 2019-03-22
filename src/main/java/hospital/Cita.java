7786076b8c3ed9775f9d0b6e3fcf1e64e700854f
Alba <alba.rodriguezsa@elorrieta-errekamari.com>
2019-03-22 14:19:57 +0100

    ...

--- src/main/java/hospital/Cita.java
+++ src/main/java/hospital/Cita.java
@@ -7,6 +7,46 @@ public class Cita {
 	private String fechaCita;
 	private String horaCita;
 	
+	public Cita(Paciente pacienteCita, Medico medicoCita, String fechaCita, String horaCita) {
+		super();
+		this.pacienteCita = pacienteCita;
+		this.medicoCita = medicoCita;
+		this.fechaCita = fechaCita;
+		this.horaCita = horaCita;
+	}
+
+	public Paciente getPacienteCita() {
+		return pacienteCita;
+	}
+
+	public void setPacienteCita(Paciente pacienteCita) {
+		this.pacienteCita = pacienteCita;
+	}
+
+	public Medico getMedicoCita() {
+		return medicoCita;
+	}
+
+	public void setMedicoCita(Medico medicoCita) {
+		this.medicoCita = medicoCita;
+	}
+
+	public String getFechaCita() {
+		return fechaCita;
+	}
+
+	public void setFechaCita(String fechaCita) {
+		this.fechaCita = fechaCita;
+	}
+
+	public String getHoraCita() {
+		return horaCita;
+	}
+
+	public void setHoraCita(String horaCita) {
+		this.horaCita = horaCita;
+	}
+	
 	
 	
 }
--- src/main/java/hospital/FuncionesHospital.java
+++ src/main/java/hospital/FuncionesHospital.java
@@ -5,7 +5,8 @@ public class FuncionesHospital {
 	public void pedirCita(String servicioIntroducido, Medico [] medico) {
 		for(int i=0; i<medico.length; i++) {
 			if(servicioIntroducido.equals(medico[i].getServicio())){
-			
+			 //introducir Datos cita
+				
 			}
 		}
 		
