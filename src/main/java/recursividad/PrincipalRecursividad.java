7c5ee0ae76e33d454202f2e95661ff01835ec0ab
elorrietaa <alba.rodriguezsa@elorrieta-errekamari.com>
2019-03-26 23:28:06 +0100

    recursividad

--- src/main/java/recursividad/PrincipalRecursividad.java
+++ src/main/java/recursividad/PrincipalRecursividad.java
@@ -3,11 +3,59 @@ package recursividad;
 public class PrincipalRecursividad {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		Recursividad1 recursividad1 = new Recursividad1();
+		
+		//Ejercicio0. Crear un programa recursivo que escriba el número factorial de un número n.
+		System.out.println("EJERCICIO 0: FACTORIAL:");
 		int resultado1= recursividad1.Factorial(3);
 		System.out.println(resultado1);
+		
+		//Ejercicio1. Crear un programa recursivo que escriba los primeros 50 numeros naturales.
+		System.out.println("EJERCICIO 1: Primeros 50 numeros naturales.");
+		recursividad1.Primeros50Num(50);
+		
+		//Ejercicio3. Crear un programa que devuelva la suma de los primeros n numeros naturales
+		System.out.println("EJERCICIO 2: suma de los primeros n numeros naturales.");
+		int sumaNNumeros=recursividad1.SumaNNumeros(4);
+		System.out.println(sumaNNumeros);
+		
+		//Ejercicio3. Crear un programa que devuelva la suma de los primeros n numeros naturales
+		System.out.println("EJERCICIO 3: Serie de fibonacci de los primeros n elementos.");
+		int resultadoFibonacci = recursividad1.Fibonacci(6);
+		System.out.println(resultadoFibonacci);
+		
+		//Ejercicio4. Crear un programa que muestre por pantalla los elementos de un array de numeros enteros.
+		System.out.println("EJERCICIO 4: array de numeros enteros.");
+		int [] cadenaEnteros= {2,5,8,4,5,6};
+		recursividad1.Cadena(cadenaEnteros);
+		
+		//Ejercicio5. Crea una funcion recursiva que devuelva el numero de digitos de un numero cualquiera. 
+		System.out.println("EJERCICIO 5: devuelva el numero de digitos de un numero cualquiera.");
+		int numero=4321;
+		int numeroDigitos=0;
+		numeroDigitos=recursividad1.DevolverNumDigitos(numero);
+		System.out.println("Número de dígitos del número: "+numero+" es: "+ numeroDigitos);
+		
+//MAAAAAAAAAAl//Ejercicio6. Crea una funcion recursiva que devuelva la suma de los digitos de un numero cualquiera. 
+		System.out.println("EJERCICIO 6: devuelva la suma de los digitos de un numero cualquiera.");
+		int sumaDigitos=0;
+		sumaDigitos=recursividad1.DevolverSumaDigitos(numero);
+		System.out.println("Suma de dígitos del número: "+numero+" es: "+ sumaDigitos);
+		
+		//Ejercicio7. Crea una programa recursivo que muestre el maximo de un array de numeros enteros cualquiera.
+		System.out.println("EJERCICIO 7: que muestre el maximo de un array de numeros enteros cualquiera.");
+		int [] cadenaEnterosDos= {2,5,4,5,6,8};
+		//recursividad1.MaximoDeLaCadena(cadenaEnterosDos);
+	
+		//correccion clase:
+			recursividad1.MostrarMaximo(cadenaEnterosDos);
+			
+		//Ejercicio8. Crea una funcion recursiva que devuelva un string del reves
+		String palabra="Hola";
+		String palabraAlreves;
+		palabraAlreves=recursividad1.reverse(palabra);
+		System.out.println(palabraAlreves);
+		
 	}
 
 }
--- src/main/java/recursividad/Recursividad1.java
+++ src/main/java/recursividad/Recursividad1.java
@@ -17,10 +17,119 @@ public class Recursividad1 {
 			System.out.println(x);
 		}
 		else { //en cada vuelta se llama a sí mismo el método. 
-			
 			System.out.println(x);
 			Primeros50Num(x-1);
 		}
 		
 	}
+	
+	public int SumaNNumeros(int n) {
+		if(n==1) {
+			return 1;
+		}
+		else {
+			return n= n+SumaNNumeros(n-1);
+		}
+	}
+	
+	public int Fibonacci(int n) {
+		if(n<2) {
+			return n; 
+		}
+		else {
+			return Fibonacci(n-1) + Fibonacci(n-2);
+		}
+	}
+	public int [] borrarPrimeraPosicion(int[] numeros) {
+		int [] numerosNuevo = new int [numeros.length-1];
+		for(int i=0; i<numeros.length-1; i++) {
+			numerosNuevo [i] = numeros[i+1];
+		}
+		/*	
+    	System.out.println("El array numerosNuevo después de eliminar una posición se queda:");
+    	for(int i=0;i<numerosNuevo.length;i++) {
+    		System.out.println(numerosNuevo[i]);
+    	}
+    	*/
+		return numerosNuevo;
+	}
+	public void Cadena(int[] numeros) {
+		int max=numeros.length;
+		if(numeros.length==1) {
+			System.out.println(numeros[0]);
+		}
+		else {
+			System.out.println(numeros[0]);
+			numeros=borrarPrimeraPosicion(numeros);
+			Cadena(numeros);
+		}
+	}
+	
+	public int DevolverNumDigitos(int numero) {
+		if(numero/10<1) {
+			return 1;
+		}
+		else {
+			System.out.println(numero);
+			return 1+DevolverNumDigitos(numero/10);
+		}
+	}
+//maaaaaaaaaaaaaaaal	
+	public int DevolverSumaDigitos(int numero) {
+		if((numero%10)*10<1) {
+			System.out.println("resultadooo "+numero);
+			return numero;
+		}
+		else {
+			System.out.println(numero);
+			return numero+DevolverSumaDigitos((numero%10)*10);
+		}
+	}
+	
+/*	MAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAL
+ * public void MaximoDeLaCadena(int[] numeros) {
+		int maxLongitud=numeros.length;
+		if(numeros.length==1) {
+			System.out.println(numeros[1]+"es mayor");
+		}
+		else {
+			if(numeros[0]>numeros[1]) {
+				numeros[1]=numeros[0];
+				numeros=borrarPrimeraPosicion(numeros);
+				System.out.println(numeros[1]+"es mayor");
+			}
+			else {
+				numeros=borrarPrimeraPosicion(numeros);
+				MaximoDeLaCadena(numeros);
+				System.out.println(numeros[1]+"es mayor");
+			}
+			
+			
+		}
+	}
+*/	
+	public void MostrarMaximo(int[] lista) {
+		
+		if(lista.length==1) {
+			System.out.println(lista[0]+"es mayor");
+		}
+		else {
+			if(lista[0]>lista[1]) {
+				lista[1]=lista[0];
+			}
+			lista=borrarPrimeraPosicion(lista);
+			MostrarMaximo(lista);
+			
+		}
+	}
+//mal	
+	public String reverse(String palabra) {
+		int palabramedida=palabra.length();
+		 if (palabra.length() == 1)
+		   return palabra;
+		 else 
+		   return reverse(palabra.substring(1))+palabra;
+		}
+
+	
 }
